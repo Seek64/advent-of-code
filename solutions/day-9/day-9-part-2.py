21decedb6f3983ba784dba8811d6587f8977f61a
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2023-12-12 14:56:54 +0100

    Added part 2 for day 9

---
+++ solutions/day-9/day-9-part-2.py
@@ -0,0 +1,27 @@
+def extrapolate(input_seq):
+    sequences = [input_seq]
+    while set(sequences[-1]) != {0}:
+        curr_seq = sequences[-1]
+        sequences.append([curr_seq[i] - curr_seq[i - 1] for i in range(1, len(curr_seq))])
+
+    sequences[-1].append(0)
+
+    for i in range(len(sequences) - 2, -1, -1):
+        sequences[i].append(sequences[i][-1] + sequences[i + 1][-1])
+
+    return sequences[0][-1]
+
+
+f = open("input.txt", "r")
+input_str = f.read()
+f.close()
+
+
+input_seq_list = [[int(n) for n in s.split()] for s in input_str.split("\n")]
+result = 0
+
+# to extrapolate backwards, simply revert the sequence and use the same functionality
+for seq in input_seq_list:
+    result += extrapolate(seq[::-1])
+
+print(result)
