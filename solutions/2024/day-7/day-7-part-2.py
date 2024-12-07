e15ab6adf1676f9ea91956bb8debdcb37c7635bb
Lucas Deutschmann <lucas.deutschmann@web.de>
2024-12-07 10:27:40 +0100

    Added solution for day 7

---
+++ solutions/2024/day-7/day-7-part-2.py
@@ -0,0 +1,22 @@
+import re
+
+with open("input.txt", "r") as f:
+    input_str = f.read()
+
+
+def find_combination(tgt: int, intermediate_result: int, op: list[int]) -> bool:
+    if not op:
+        return intermediate_result == tgt
+    n, *op = op
+    return (find_combination(tgt, intermediate_result + n, op)
+            or find_combination(tgt, intermediate_result * n, op)
+            or find_combination(tgt, int(str(intermediate_result) + str(n)), op))
+
+
+result = 0
+for equation_str in input_str.split("\n"):
+    target, *operands = map(int, re.findall(r"\d+", equation_str))
+    if find_combination(target, operands[0], operands[1:]):
+        result += target
+
+print(result)
